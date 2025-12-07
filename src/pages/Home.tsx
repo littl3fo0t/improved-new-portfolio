45f226b2fe564f81407336de967a4e7b4b622faa
littl3fo0t <thomas.brun.1703@hotmail.com>
2025-12-06 21:31:43 -0400

    Implemented avatar on Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@
 
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import avatar from "../assets/images/avatar.png";
 
 const Home = () => {
     return (
@@ -12,8 +13,12 @@ const Home = () => {
             <main className="container has-text-centered">
                 <h1 className="title is-spaced is-size-1-desktop is-size-2-tablet is-size-3-mobile">Application Developer, Systems Analyst & Aspiring Full-Stack Developer</h1>
                 <h2 className="subtitle is-spaced is-size-2-desktop is-size-3-tablet is-size-4-mobile">Hi, I'm Thomas 👋</h2>
-                <figure className="image container is-128x128">
-                    <img src="https://bulma.io/assets/images/placeholders/128x128.png" alt="" className="is-rounded is-spaced" />
+                <figure className="image container" id="avatar">
+                    <img 
+                        src={avatar} 
+                        alt="Avatar image" 
+                        className="is-rounded is-spaced"
+                    />
                 </figure>
             </main>
 
